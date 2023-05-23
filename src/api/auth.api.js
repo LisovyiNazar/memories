329af4ee8e8c088bd5bc79157bda5f7bc82a88fb
Назар Лісовий <nazarlisovij@forstnazar.local>
2023-05-23 21:18:25 +0300

    fix proxy

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-export const googleLogin = (userData) => axios.post('/auth/google-login', userData)
-export const login = (userData) => axios.post('/auth/login', userData)
-export const register = (userData) => axios.post('/auth/register', userData)
+export const googleLogin = (userData) => axios.post('https://memories-api-rs27.onrender.com/auth/google-login', userData)
+export const login = (userData) => axios.post('https://memories-api-rs27.onrender.com/auth/login', userData)
+export const register = (userData) => axios.post('https://memories-api-rs27.onrender.com/auth/register', userData)
--- src/api/posts.api.js
+++ src/api/posts.api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const fetchPosts = (page) => axios.get(`/posts/${page}`)
-export const fetchUserPosts = (nickname) => axios.get(`/posts/user/${nickname}`)
-export const fetchLikedPosts = (nickname) => axios.get(`/posts/liked/user/${nickname}`)
-export const createPost = (newPost) => axios.post('/posts', newPost)
-export const updatePost = (id, postData) => axios.patch(`/posts/${id}`, postData)
-export const deletePost = (id) => axios.delete(`/posts/${id}`)
-export const likePost   = (id, userId) => axios.patch(`/posts/like/${id}`, {userId: userId})
+export const fetchPosts = (page) => axios.get(`https://memories-api-rs27.onrender.com/posts/${page}`)
+export const fetchUserPosts = (nickname) => axios.get(`https://memories-api-rs27.onrender.com/posts/user/${nickname}`)
+export const fetchLikedPosts = (nickname) => axios.get(`https://memories-api-rs27.onrender.com/posts/liked/user/${nickname}`)
+export const createPost = (newPost) => axios.post('https://memories-api-rs27.onrender.com/posts', newPost)
+export const updatePost = (id, postData) => axios.patch(`https://memories-api-rs27.onrender.composts/${id}`, postData)
+export const deletePost = (id) => axios.delete(`https://memories-api-rs27.onrender.com/posts/${id}`)
+export const likePost   = (id, userId) => axios.patch(`https://memories-api-rs27.onrender.com/posts/like/${id}`, {userId: userId})
